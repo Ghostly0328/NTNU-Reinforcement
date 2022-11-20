d64b7912c4b990cad1be3ec5ae5e3c7e7e0bccf1
Ghostly0328 <87267435+Ghostly0328@users.noreply.github.com>
2022-11-21 00:48:37 +0800

    Assingment_7 update

--- Assignment_7/Qlearning_CustomWorld.py
+++ Assignment_7/Qlearning_CustomWorld.py
@@ -2,11 +2,15 @@
 
 import gym
 import numpy as np
+from gym.wrappers import FlattenObservation
+from gym_examples.wrappers import RelativePosition
 import time
 import math
 import matplotlib.pyplot as plt
 
-env = gym.make("Taxi-v3")#, render_mode="human")
+# env = gym.make("Taxi-v3")#, render_mode="human")
+env = gym.make('gym_examples/Assignment-v0', size=5)
+env = FlattenObservation(env)
 
 def plot(rewards):
     plt.figure(2)
@@ -43,14 +47,16 @@ def normalize(list):
     return list 
 
 def Qlearning(alpha, gamma, epsilon, episodes, max_steps, EPS_START, EPS_END, EPS_DECAY, n_tests):
-    n_states, n_actions = env.observation_space.n, env.action_space.n
-    print(n_actions)
+    n_states, n_actions = env.observation_space_state, env.action_space.n
+    
+    # print(n_states, n_actions)
     Q = Q_value_initialize(n_states, n_actions, type = 0)
     timestep_reward = []
     for episode in range(episodes):
         print(f"Episode: {episode}")
         s, info = env.reset() # read also state
-
+        s = env.nplinspace[s[0],s[1],s[2],s[3],s[4],s[5]]
+        
         EPS_START = 0.001
         EPS_END = 1
         EPS_DECAY = 10 
@@ -66,20 +72,23 @@ def Qlearning(alpha, gamma, epsilon, episodes, max_steps, EPS_START, EPS_END, EP
             # -1 per step unless other reward is triggered.
             # +20 delivering passenger.
             # -10 executing “pickup” and “drop-off” actions illegally.
+            
             s_, reward, terminated, truncated, info = env.step(a)
-
+            state = env.nplinspace[s_[0],s_[1],s_[2],s_[3],s_[4],s_[5]]
+            
             #s_, reward, done, info = env.step(a)
             total_reward += reward
-            a_next = np.argmax(Q[s_, :]).item()
+            a_next = np.argmax(Q[state, :]).item()
 
             if terminated or truncated:
                 Q[s, a] += alpha * (reward - Q[s, a])
             else:
-                Q[s, a] += alpha * (reward + (gamma * Q[s_, a_next]) - Q[s, a])
-            s, a = s_, a_next
+                Q[s, a] += alpha * (reward + ( Q[state, a_next]) - Q[s, a])
+            s, a = state, a_next
             
             if terminated or truncated:
                 s, info = env.reset()
+                s = env.nplinspace[s[0],s[1],s[2],s[3],s[4],s[5]]
 
         timestep_reward.append(total_reward/t)
 
@@ -95,15 +104,20 @@ def Qlearning(alpha, gamma, epsilon, episodes, max_steps, EPS_START, EPS_END, EP
 
 #----------------------------------------------------
 def test_agent(Q, n_tests = 0, delay=1):
-    env = gym.make('Taxi-v3', render_mode="human")
+    # env = gym.make('Taxi-v3', render_mode="human")
+    env = gym.make('gym_examples/Assignment-v0', render_mode="human", size=5)
+    env = FlattenObservation(env)
+    
     for testing in range(n_tests):
         print(f"Test #{testing}")
         s, info = env.reset()
+        s = env.nplinspace[s[0],s[1],s[2],s[3],s[4],s[5]]
         while True:
             time.sleep(delay)
             a = np.argmax(Q[s, :]).item()
             print(f"Chose action {a} for state {s}")
             s, reward, terminated, truncated, info = env.step(a)
+            s = env.nplinspace[s[0],s[1],s[2],s[3],s[4],s[5]]
             #time.sleep(1)
 
             if terminated or truncated:
@@ -115,7 +129,7 @@ if __name__ == "__main__":
     alpha = 0.1 # learning rate
     gamma = 0.8 # discount factor
     epsilon = 0.01 # epsilon greedy exploration-explotation (smaller more random)
-    episodes = 200
+    episodes = 100
     
     EPS_START = 0.001
     EPS_END = 1
